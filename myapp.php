6dbf6b2407685d9927bb3b12305073b3f82082f7
Calvins Ongele <48557897+calvins-ongele@users.noreply.github.com>
2026-09-14 15:05:21 +0300

    Add CustomFunctions class and MyApp methods

---
+++ myapp.php
@@ -0,0 +1,82 @@
+<?php
+class CustomFunctions {
+  
+    public static function cleanInput($data, $type = 'string') {
+        //basic sanitization
+        $data = trim($data);
+
+        //zero is not empty
+        if ($data === '0') return '0';
+        if (empty($data)) return false;
+
+        switch ($type) {
+            case 'email':
+                $data = filter_var($data, FILTER_SANITIZE_EMAIL);
+                return filter_var($data, FILTER_VALIDATE_EMAIL) ? $data: false;
+            case 'int':
+                return filter_var($data, FILTER_VALIDATE_INT);
+            case 'url':
+                $data = filter_var($data, FILTER_SANITIZE_URL);
+                return filter_var($data, FILTER_VALIDATE_URL) ? $data: false;
+            case 'float':
+                return filter_var($data, FILTER_VALIDATE_FLOAT); 
+            case 'string':
+            default:
+                return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+        }
+
+    }
+}
+
+class MyApp {
+
+
+	/**
+	 * mysql data 
+	 * 
+	 * CREATE TABLE cart(
+			id int AUTO_INCREMENT primary key,
+			item_id int not null,
+			cookie_id varchar(100),
+			item_count int,
+			created_at datetime DEFAULT CURRENT_TIMESTAMP,
+			updated_at datetime ON UPDATE CURRENT_TIMESTAMP,
+
+			-- foreign key
+			FOREIGN KEY (item_id) REFERENCES products(id) ON DELETE CASCADE
+		);
+	 * 
+	 * 
+	 */
+	public function fetchcart(){ 
+		// clean post data
+		$cookieId = CustomFunctions::cleanInput($_POST['cookie_id'], 'string');
+
+		// own data fetch way
+		$data = $this->_get('cart', 'cookie_id', [ $cookieId ]);
+		echo json_encode([
+			'total'=>$data[0],
+			'cart'=>$data[1],
+		]);
+	} 
+	public function savecart(){ 
+		// clean post data
+		$cookieId = CustomFunctions::cleanInput($_POST['cookie_id'], 'string');
+
+		if (!is_numeric($_POST['item_id'] )){
+			die(json_encode(['error'=>true, 'msg'=>'Invalid Item ID']));
+		}
+
+		$data = $this->_get('cart', 'cookie_id,item_id', [$cookieId, $_POST['item_id']], 0);
+
+		if ($data[0] > 0) {
+			//update count
+			$this->_update("cart", 'item_count', 'cookie_id,item_id', [$data[1]['item_count']+1, $cookieId, $_POST['item_id']] );
+		} else {
+			$this->_insert("cart", 'item_count, cookie_id,item_id', [1, $cookieId, $_POST['item_id']] );
+		}
+
+		$this->fetchcart();
+	} 
+
+}
